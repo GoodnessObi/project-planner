33fdd07c502dc7f433a6ccff5bdea363194ca104
GoodnessObi <goodnessobi@gmail.com>
2021-09-20 22:11:19 +0100

    chore: install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { BrowserRouter } from 'react-router-dom';
+
 function App() {
   return (
-    <div className="App">
-     <p>Running!!!!</p>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <p>Running!!!!</p>
+      </div>
+    </BrowserRouter> 
   );
 }
 
